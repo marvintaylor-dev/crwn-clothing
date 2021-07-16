856fe4ce379ea1204d6eadb92521b9f7a713d2e4
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-07-16 15:41:14 -0700

    added cart icon and cart-dropdown, with cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { connect } from 'react-redux';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to='/'>
             <Logo className="logo" />
@@ -24,24 +26,26 @@ const Header = ({ currentUser }) => (
                 to='/contact'>
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                    <div
-                        className='option'
-                        onClick={() => auth.signOut()}>
-                        SIGN OUT
-                    </div>
-                    :
+            {currentUser ? (
+                <div
+                    className='option'
+                    onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
                     <Link className='option' to='/signin'>
                         SIGN IN
                     </Link>
-            }
+                )}
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
