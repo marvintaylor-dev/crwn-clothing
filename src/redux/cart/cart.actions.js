bc08e0158c04875439a48c525ec5dcc30ba6fe34
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-07-16 17:00:43 -0700

    addItem added to cart.actions and to collection items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
     //No payload for this. Just toggling an option
 });
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
