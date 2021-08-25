5ed40203a79eba19a087934a277211b1428a61c3
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-08-25 16:04:40 -0700

    updated shop data with new data, cart utils groups items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,21 @@
+//Logic for not replicating occurrences of items in the shopping cart. Add a number of a certain item instead of creating a new object for every time it's added.
+
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    //will find the first item in the list of items whose ID matches the ID just added.
+    //Save that value as existing cart item
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    //if the item does already exist
+    if (existingCartItem) {
+        //return a new array  via map so that our state renders properly. 
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem)
+    }
+
+    //if there's no existing cart item. return cart items and a quantity of 1
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
